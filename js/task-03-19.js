125cec7a37b1be4b2f81160e82125f245c4c18df
OleksandrPenskyi <suchgoodidea@gmail.com>
2020-12-20 12:09:18 +0200

    Added solve for tasks #19-23

--- js/task-03-19.js
+++ js/task-03-19.js
@@ -1,3 +1,33 @@
-
+// Задача. Коллекция значений свойства
+// Задание
+// Напиши функцию getAllPropValues(propName) которая принимает один параметр propName - имя(ключ) свойства.
+// Функция должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products.
+// Если в объектах нет свойства с таким именем, функция должна вернуть пустой массив.
 
 // Решение
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Пиши код ниже этой строки
+  const valueArr = [];
+
+  // перебираем массив по обьектам
+  for (const item of products) {
+    // Делаем массив из ключей каждого обьекта
+    // Проверяем эти массивы на наличие свойства, которое передаем в скобках в ф-ю
+    if (Object.keys(item).includes(propName)) {
+      valueArr.push(item[propName]);
+    }
+  }
+
+  return valueArr;
+  // Пиши код выше этой строки
+}
+
+console.log(getAllPropValues("quantity"));
+// Вызов getAllPropValues('quantity') возвращает [4, 3, 7, 9].
--- js/task-03-20.js
+++ js/task-03-20.js
@@ -1,3 +1,37 @@
-
+// Задача. Общая стоимость товара
+// Задание
+// Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара.
+// Функция должна вернуть общую стоимость(цена * количество) товара с таким именем из массива products.
 
 // Решение
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+
+  // Перебираем массив по обьектно
+  for (const item of products) {
+    // console.log(item);
+
+    //   если в обьекте элемент с именем === имени которое мы задаем в функцию,
+    //       то возращаем результат умножения этого товара на цены на кол-во
+    if (item.name === productName) {
+      return item.price * item.quantity;
+    }
+
+    // if (item.name.includes(productName)) {
+    //   return item.price * item.quantity;
+    // }
+  }
+
+  return 0;
+  // Пиши код выше этой строки
+}
+
+console.log(calculateTotalPrice("Радар"));
+// Вызов calculateTotalPrice('Радар') возвращает 5200.
