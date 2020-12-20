79c2e250143d71b037be20276901cab366ef145d
Krytoryni <suchgoodidea@gmail.com>
2020-12-20 21:31:53 +0200

    Added solve for tasks #27-28

--- js/task-03-26.js
+++ js/task-03-26.js
@@ -12,14 +12,16 @@
 // Решение
 // Пиши код ниже этой строки
 function calculateMeanTemperature(forecast) {
-//   const todayLow = forecast.today.low;
-//   const todayHigh = forecast.today.high;
-//   const tomorrowLow = forecast.tomorrow.low;
-//   const tomorrowHigh = forecast.tomorrow.high;
+  //   const todayLow = forecast.today.low;
+  //   const todayHigh = forecast.today.high;
+  //   const tomorrowLow = forecast.tomorrow.low;
+  //   const tomorrowHigh = forecast.tomorrow.high;
 
-    const {today:{low: todayLow, high:todayHigh}, tomorrow:{low: tomorrowLow, high: tomorrowHigh} } = forecast;  
-    
-    
+  const {
+    today: { low: todayLow, high: todayHigh },
+    tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+  } = forecast;
+
   // Пиши код выше этой строки
   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
 }
--- js/task-03-27.js
+++ js/task-03-27.js
@@ -1,3 +1,12 @@
-
+// Задание
+// В переменной scores хранится массив результатов тестирования. Используя распыление и методы Math.max() и Math.min() дополни код так, чтобы в переменной bestScore был самый высокий балл, а в worstScore самый низкий.
 
 // Решение
+// Значение переменной scores это массив [89, 64, 42, 17, 93, 51, 26].
+const scores = [89, 64, 42, 17, 93, 51, 26];
+// Пиши код ниже этой строки
+const bestScore = Math.max(...scores);
+const worstScore = Math.min(...scores);
+
+console.log(bestScore);
+console.log(worstScore);
