10ca48e0bbabb313fcc13923cf1da0d70e67e1bd
OleksandrPenskyi <suchgoodidea@gmail.com>
2021-01-15 16:16:32 +0200

    added solve for tasks #11-16

--- js/task-06-15.js
+++ js/task-06-15.js
@@ -1,1 +1,21 @@
+// Дополни код так, чтобы в переменной evenNumbers
+// получился массив чётных чисел из массива numbers,
+// а в переменной oddNumbers массив нечётных.Обязательно используй метод filter().
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Пиши код ниже этой строки
+
+// const evenNumbers = numbers;
+// const oddNumbers = numbers;
+
 // Решение
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// Пиши код ниже этой строки
+
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+const oddNumbers = numbers.filter(number => !(number % 2 === 0));
+
+console.log(evenNumbers);
+console.log(oddNumbers);
+// Значение переменной evenNumbers это массив [24, 82, 36, 18, 52].
+// Значение переменной oddNumbers это массив [17, 61, 47, 73].
