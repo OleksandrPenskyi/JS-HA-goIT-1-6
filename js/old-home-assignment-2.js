7032574a60b2969503a8edf9d8ad32e6524a66b6
OleksandrPenskyi <suchgoodidea@gmail.com>
2021-01-19 16:47:41 +0200

    added alternative solve for old ha-2

--- js/old-home-assignment-2.js
+++ js/old-home-assignment-2.js
@@ -300,4 +300,55 @@
 // addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
 // console.log(`После добавления Вашего логина, массив имен такой: ${logins}`);
+
+// *Вариант № 2
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+const isLoginValid = function (login) {
+  return login.length >= 4 && login.length < 16;
+};
+
+const isLoginUnique = function (allLogins, login) {
+  return allLogins.includes(login);
+};
+
+const addLogin = function (allLogins, login) {
+  // if (isLoginValid(login)) {
+  //   console.log(`${login} - валидный`);
+  // } else {
+  //   return `Ошибка! Логин: ${login} должен быть от 4 до 16 символов`;
+  // }
+
+  // if (!isLoginUnique(allLogins, login)) {
+  //   console.log(`${login} - уникальный`);
+  // } else {
+  //   return `Логин: ${login} уже используется!`;
+  // }
+
+  // if (isLoginValid(login) && !isLoginUnique(allLogins, login)) {
+  //   logins.push(login);
+  //   return `login: ${login} прошел все проверки и добавлен в массив!`;
+  // }
+
+  if (isLoginValid(login) && !isLoginUnique(allLogins, login)) {
+    logins.push(login);
+    return `login: "${login}" прошел все проверки и добавлен в массив!`;
+  } else {
+    return `login: "${login}" не прошел проверки!`;
+  }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+
+console.log(`Массив до: ${logins.length}`);
+
+console.log(addLogin(logins, "Ajax")); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, "robotGoogles")); // 'Такой логин уже используется!'
+console.log(addLogin(logins, "Zod")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, "jqueryisextremelyfast")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+// console.log(isLoginUnique('robotGoogles'));
+console.log(`Массив после: ${logins.length}`);
 // !END Задание 7
